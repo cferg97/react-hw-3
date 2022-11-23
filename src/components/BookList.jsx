0b7a882866ca1dcea611b68e8a8f150bab1b5400
Catriona Ferguson <114424301+cferg97@users.noreply.github.com>
2022-11-23 20:44:02 +0000

    Update BookList.jsx

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,48 +1,40 @@
 import SingleBook from "./SingleBook";
-import { Row, Col, Container, Form, } from "react-bootstrap"
+import { Row, Col, Container, Form, FormControl } from "react-bootstrap"
 import { Component } from "react"
 
 
 class BookList extends Component {
 
     state = {
-        searchQuery: "",
-        books: this.props.ListOfBooks
+        searchQuery: ""
     }
 
-    FilterBookList = (value) => {
-        this.setState({
-            searchQuery: value,
-        })
-
-        let searchQuery = this.state.searchQuery
-        this.setState({
-            books: this.state.books.filter(function (book) {
-                return book.title.toLowerCase().includes((searchQuery).toLowerCase())
-            })
-        })
-        console.log(searchQuery)
-    }
 
     render(){
     return (
         <Container>
-        <Form.Group>
-        <Form.Control
-        type="text"
-        placeholder="Search for books"
-        className="me-2"
-        aria-label="Search"
-        value={this.state.searchQuery}
-        onChange={(event) => this.FilterBookList(event.target.value)}
-        />
-            </Form.Group>
-            <h2>Displaying {this.state.books.length} of {this.props.ListOfBooks.length} </h2>
-            <Row>
-                {this.state.books.map((book) => (
-                    <Col key={book.asin} sm={6} md={4}><SingleBook book={book}/></Col>
-                ))}
-            </Row>
+            <Form className="d-flex w-100">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="mr-2"
+                  aria-label="Search"
+                  onChange={(e) =>
+                    this.setState({ searchQuery: e.target.value })
+                  }
+                />
+              </Form>
+              <Row>
+          {this.props.ListOfBooks
+            .filter((element) =>
+              element.title.toLowerCase().includes(this.state.searchQuery)
+            )
+            .map((element) => (
+              <Col xs={4} md={3} className="mt-2 mb-2">
+                <SingleBook book={element} />
+              </Col>
+            ))}
+        </Row>
         </Container>
     )
     }
